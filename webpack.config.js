2ab576f9571ed035a65d598da8f636bf893aefa1
RahulSG39 <rahulgirish39@gmail.com>
2021-06-17 13:31:41 +0530

    Added documentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./index.js", //Provides an entry way into the project. Can take many entries.
   output: {
-    filename: "bundle.js",
-    path: __dirname + "/dist",
+    filename: "bundle.js", //Filename that is used to store the output of the webpack. Convention is to use bundle.js.
+    path: __dirname + "/dist", //Provides path where the bundle.js should be stored. Only takes in the absolute path therefore use __dirname.
   },
   module: {
     rules: [
       {
-        test: /\.js%/,
-        exclude: /(node_modules)/,
-        use: "babel-loader",
+        test: /\.js%/, //Regular expression which searches for js files.
+        exclude: /(node_modules)/, //Excludes node modules from being searched
+        use: "babel-loader", //makes the js files backward compatible.
       },
       {
-        test: /\.scss$/,
+        test: /\.scss$/, //Regular expression which searches for scss files.
         use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          // The modules are used from bottom to top
+          MiniCssExtractPlugin.loader, //Used to extract the css from the javascript inside the bundle.js
+          "css-loader", //Loads the css into the bundle.js produced by webpack.
+          "postcss-loader", //Converts css into intermediary object so it can be changed and then converts it back to css.
+          "sass-loader", //Coverts scss code into css
         ],
       },
     ],
@@ -27,7 +28,7 @@ module.exports = {
   plugins: [
     new MiniCssExtractPlugin({
       filename: "main.css",
-    }),
+    }), // Creates an instance of the minicssextract plugin and stores the output from css-loader into a separate file.
   ],
-  watch: true,
+  watch: true, //Used to avoid reloading and watches for changed. Works as hot reload.
 };
